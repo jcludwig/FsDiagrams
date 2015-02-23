9091c60f501aa7db89ca3ce8cbd997954ccffedf
jolud <jolud@microsoft.com>
2015-02-22 16:49:40 -0800

    Use VisualStudio test framework. Implement svg writing for lines.

--- FsDiagramsLib/Svg.fs
+++ FsDiagramsLib/Svg.fs
@@ -14,10 +14,21 @@ type Color =
     | NamedColor of string
     | HexRgb of string
     | Rgb of byte * byte * byte
-    | RgbPercentage of float * float * float
+    | RgbPercentage of decimal * decimal * decimal
+    with
+    override this.ToString() =
+        match this with
+            | NamedColor c -> c
+            | HexRgb c -> c
+            | Rgb (r, g, b) -> sprintf "rgb(%i, %i, %i)" r g b
+            | RgbPercentage (r, g, b) -> sprintf "rgb(%M%%, %M%%, %M%%)" r g b
 
 type LineCap =
     | Round
+    with
+    override this.ToString() =
+        match this with
+            | Round -> "round"
     
 type LineData = {
     x1 : int;
@@ -42,10 +53,55 @@ type GraphicsElement =
     | Image
     | Use
 
-type SvgDocument() =
+type Element =
+    | GraphicsElement of GraphicsElement
+
+type Document = {
+    Elements : Element list
+}
+
+type SvgWriter() =
     let xdoc = new XDocument()
     let svgns = XNamespace.Get(@"http://www.w3.org/2000/svg")
     let svgroot = xdoc.Add(new XElement(svgns + "svg"))
 
+    let xname (name:string) =
+        XName.Get(name)
+
+    let WriteLine (data:LineData) (parent:XElement) =
+        let attrs:seq<string * string> = seq {
+            yield ("x1", string data.x1)
+            yield ("y1", string data.y1)
+            yield ("x2", string data.x2)
+            yield ("y2", string data.y2)
+            match data.stroke with
+                | Some stroke -> yield("stroke", string stroke)
+                | None -> ()
+            match data.stroke_dasharray with
+                | Some dasharray -> yield("stroke-dasharray", dasharray |> Seq.map string |> String.concat ",")
+                | None -> ()
+            match data.stroke_linecap with
+                | Some linecap -> yield("stroke-linecap", string linecap)
+                | None -> ()
+        }
+        let element =
+            new XElement(
+                xname "line",
+                attrs |> Seq.map (fun (name, value) -> new XAttribute(xname name, value)) |> Seq.toArray)
+        parent.Add(element)
+
+    let WriteGraphicsElement (el:GraphicsElement) (parent:XElement) =
+        match el with
+            | Line data -> WriteLine data parent
+            | _ ->  failwith "Element not supported."
+
+    let WriteElement (el:Element) (parent:XElement) =
+        match el with
+            | GraphicsElement e -> WriteGraphicsElement e parent
+
     member public this.GetDocument() =
         xdoc
+
+    member public this.WriteDocument(doc:Document) =
+        doc.Elements
+            |> List.iter (fun el -> WriteElement el xdoc.Root)
--- FsDiagramsTest/Svg.fs
+++ FsDiagramsTest/Svg.fs
@@ -1,15 +1,46 @@
 ﻿namespace FsDiagrams.Test.Svg
 
 open FsUnit
-open NUnit.Framework
+open Microsoft.VisualStudio.TestTools.UnitTesting
 
 open FsDiagrams.Svg
 
-[<TestFixture>]
+[<TestClass>]
 type SvgTests() =
     
-    [<Test>]
+    [<TestMethod>]
     member this.``Basic document has svg root``() =
-        let xdoc = (new SvgDocument()).GetDocument()
+        let xdoc = (new SvgWriter()).GetDocument()
         xdoc.Root.Name.LocalName |> should equal "svg"
         xdoc.Root.Name.Namespace.NamespaceName |> should equal @"http://www.w3.org/2000/svg"
+
+[<TestClass>]
+type ColorTests() =
+
+    [<TestMethod>]
+    member this.``NamedColor ToString``() =
+        let color = Color.NamedColor("red")
+        color.ToString() |> should equal "red"
+        
+    [<TestMethod>]
+    member this.``HexRgb ToString``() =
+        let color = Color.HexRgb("#ff00aa")
+        color.ToString() |> should equal "#ff00aa"
+
+    [<TestMethod>]
+    member this.``Rgb ToString``() =
+        let color = Color.Rgb(255uy, 0uy, 128uy)
+        color.ToString() |> should equal "rgb(255, 0, 128)"
+
+    [<TestMethod>]
+    member this.``RgbPercentage ToString``() =
+        let color = Color.RgbPercentage(100M, 0.0M, 50.501M)
+        color.ToString() |> should equal "rgb(100%, 0.0%, 50.501%)"
+
+[<TestClass>]
+type LineCapTests() =
+
+    [<TestMethod>]
+    member this.``Round ToString``() =
+        let linecap = LineCap.Round
+        linecap.ToString() |> should equal "round"
